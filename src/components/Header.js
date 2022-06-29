c41ce9959ef286c74e88d7478f52a9dc98bda2d5
Lola Applegate <laurenemilya@gmail.com>
2022-06-29 08:56:59 -0700

    initial file & connection

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                I'm the header!
+            </h1>
+        </header>
+    )
+}
