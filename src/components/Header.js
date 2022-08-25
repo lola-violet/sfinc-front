d1b423ea14b84c8f43d50fa73cb97559a7f6937a
Lola Applegate <laurenemilya@gmail.com>
2022-08-25 12:10:25 -0700

    adding background banner image for home page & adjusting css to accomodate

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header() {
         <header className='cust-header'>
             <Navbar className='cust-nav' expand='lg'>
                 <Container>
-                    <Navbar.Brand >
+                    <Navbar.Brand className='cust-log'>
                         <Link className='navbar-brand' to='/'>
                             <img className='cust-logo img-fluid' src={transparentLogo} alt='SoilFreeze Logo' />
                         </Link>
@@ -41,39 +41,6 @@ export default function Header() {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
-            {/* <nav className='cust-nav navbar navbar-expand-lg'>
-                <div className='container'>
-                    <Link className='navbar-brand' to='/'>
-                        <img className='cust-logo img-fluid' src={transparentLogo} alt='SoilFreeze Logo' />
-                    </Link>
-                    <button className='navbar-toggler' type='button' data-bs-toggle="collapse" data-bs-target="#navbarMainCollapse" aria-controls="navbarMainCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div className='collapse navbar-collapse' id='navbarMainCollapse'>
-                        <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/'>Home</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/about'>About</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/advantage'>SoilFreeze Advantage</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/projects'>Projects</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/news'>News</Link>
-                            </li>
-                            <li className='nav-item'>
-                                <Link className='nav-link' to='/contact'>Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav> */}
         </header>
     )
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ImageSlide from '../homeComponents/ImageSlide';
 import CompanyBio from '../homeComponents/CompanyBio';
 import ProjectIcons from '../homeComponents/ProjectIcons';
+import BannerNo1 from '../../assets/img/banners/banner_1.jpg';
+
+import '../../styles/home.css';
+
 
 export default function Home() {
     return (
-        <div className='container'>
-            <h1>I'm the home page</h1>
-            <ImageSlide />
-            <CompanyBio />
-            <ProjectIcons />
+        // <div className='container'>
+        //     <h1>I'm the home page</h1>
+        //     <ImageSlide />
+        //     <CompanyBio />
+        //     <ProjectIcons />
+        // </div>
+        <>
+        <div>
+            <img src={BannerNo1} alt='banner image' className='img-fluid homeImg' />
         </div>
+        </>
+        
     )
 }
