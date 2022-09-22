1d01a7bcbe89fee956b97e37bf9782de41da2036
Lola Applegate <laurenemilya@gmail.com>
2022-09-22 16:19:22 -0700

    updated cutsheet carousel for utilities & pump stations

--- src/components/projectComponents/utilPumpStaComp/CutsheetCarousel.js
+++ src/components/projectComponents/utilPumpStaComp/CutsheetCarousel.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 // Utilities & Pump Stations
 //// Cutsheets
-import NewarkShaftRepair1201 from '../../../assets/cutsheets/shaCroPass/newark_shaft_repair-1201.png';
-import NewarkTbmReceivingShaft from '../../../assets/cutsheets/shaCroPass/newark_tbm_receiving_shaft.png';
-import PierSLongBeach from '../../../assets/cutsheets/shaCroPass/pier-s_long_beach.png';
-import TurkeyCreekTunnel from '../../../assets/cutsheets/shaCroPass/turkey_creek_tunnel.png';
+import BnsfPipelineCasingTrench from '../../../assets/cutsheets/utilPumpSta/bnsf_pipeline_casing_trench.png';
+import CedarLandingPumpStation from '../../../assets/cutsheets/utilPumpSta/cedar_landing_pump_station.png';
+import MallardBayPumpStationFoundations from '../../../assets/cutsheets/utilPumpSta/mallard_bay_pump_station-foundations.png';
+import MinnesotaOilPipelineRepair from '../../../assets/cutsheets/utilPumpSta/minnesota_oil_pipeline_repair.png';
+import OlympicPipelineRelocation from '../../../assets/cutsheets/utilPumpSta/olympic_pipeline_relocation.png';
+import Terminal5PipeRepair from '../../../assets/cutsheets/utilPumpSta/terminal_5_pipe_repair.png';
+import TurlockCaRawWaterPumpStation from '../../../assets/cutsheets/utilPumpSta/turlock_ca_raw_water_pump_station.png';
 
 export default function CutsheetCarousel() {
     return (
         <div>
-            <div id='shaCroPassCarousel' className='carousel carousel-dark slide customCarou' data-bs-ride='carousel' data-bs-pause='false'>
+            <div id='utilPumpStaCarousel' className='carousel carousel-dark slide customCarou' data-bs-ride='carousel' data-bs-pause='false'>
                 <div className='carousel-indicators'>
                     <button
                         type='button'
-                        data-bs-target='#shaCroPassCarousel'
+                        data-bs-target='#utilPumpStaCarousel'
                         data-bs-slide-to='0'
                         className='active'
                         aria-current='true'
@@ -22,57 +25,96 @@ export default function CutsheetCarousel() {
                     ></button>
                     <button
                         type='button'
-                        data-bs-target='#shaCroPassCarousel'
+                        data-bs-target='#utilPumpStaCarousel'
                         data-bs-slide-to='1'
                         aria-label='slide 2'
                     ></button>
                     <button
                         type='button'
-                        data-bs-target='#shaCroPassCarousel'
+                        data-bs-target='#utilPumpStaCarousel'
                         data-bs-slide-to='2'
                         aria-label='slide 3'
                     ></button>
                     <button
                         type='button'
-                        data-bs-target='#shaCroPassCarousel'
+                        data-bs-target='#utilPumpStaCarousel'
                         data-bs-slide-to='3'
                         aria-label='slide 4'
+                    ></button>
+                    <button
+                        type='button'
+                        data-bs-target='#utilPumpStaCarousel'
+                        data-bs-slide-to='4'
+                        aria-label='slide 5'
+                    ></button>
+                    <button
+                        type='button'
+                        data-bs-target='#utilPumpStaCarousel'
+                        data-bs-slide-to='5'
+                        aria-label='slide 6'
+                    ></button>
+                    <button
+                        type='button'
+                        data-bs-target='#utilPumpStaCarousel'
+                        data-bs-slide-to='6'
+                        aria-label='slide 7'
                     ></button>
                 </div>
                 <div className='carousel-inner'>
                     <div className='carousel-item active'>
                         <img 
                             className='d-block w-100 custCarouImg'
-                            src={NewarkShaftRepair1201} 
-                            alt='Cutsheet for Newark Shaft Repair' 
+                            src={BnsfPipelineCasingTrench} 
+                            alt='Cutsheet for BNSF Pipeline Casing Trench' 
                         />
                     </div>
                     <div className='carousel-item'>
                         <img
                             className='d-block w-100 custCarouImg'
-                            src={NewarkTbmReceivingShaft}
-                            alt='Cutsheet for Newark TBM Receiving Shaft' 
+                            src={CedarLandingPumpStation}
+                            alt='Cutsheet for Cedar Landing Pump Station' 
                         />
                     </div>
                     <div className='carousel-item'>
                         <img
                             className='d-block w-100 custCarouImg'
-                            src={PierSLongBeach}
-                            alt='Cutsheet for Pier-S Long Beach' 
+                            src={MallardBayPumpStationFoundations}
+                            alt='Cutsheet for Mallard Bay Pump Station Foundations' 
                         />
                     </div>
                     <div className='carousel-item'>
                         <img
                             className='d-block w-100 custCarouImg'
-                            src={TurkeyCreekTunnel}
-                            alt='Cutsheet for Turkey Creek Tunnel' 
+                            src={MinnesotaOilPipelineRepair}
+                            alt='Cutsheet for Minnesota Oil Pipeline Repair' 
+                        />
+                    </div>
+                    <div className='carousel-item'>
+                        <img
+                            className='d-block w-100 custCarouImg'
+                            src={OlympicPipelineRelocation}
+                            alt='Cutsheet for Olympic Pipeline Relocation' 
+                        />
+                    </div>
+                    <div className='carousel-item'>
+                        <img
+                            className='d-block w-100 custCarouImg'
+                            src={Terminal5PipeRepair}
+                            alt='Cutsheet for Terminal 5 Pipe Repair' 
+                        />
+                    </div>
+                    <div className='carousel-item'>
+                        <img
+                            className='d-block w-100 custCarouImg'
+                            src={TurlockCaRawWaterPumpStation}
+                            alt='Cutsheet for Turlock CA Raw Water Pump Station' 
                         />
                     </div>
                 </div>
                 <button
                     className='carousel-control-prev'
                     type='button'
-                    data-bs-target='#shaCroPassCarousel'
+                    data-bs-target='#utilPumpStaCarousel'
                     data-bs-slide='prev'
                     >
                     <span className='carousel-control-prev-icon' aria-hidden='true'></span>
@@ -81,7 +123,7 @@ export default function CutsheetCarousel() {
                 <button
                     className='carousel-control-next'
                     type='button'
-                    data-bs-target='#shaCroPassCarousel'
+                    data-bs-target='#utilPumpStaCarousel'
                     data-bs-slide='next'
                     >
                     <span className='carousel-control-next-icon' aria-hidden='true'></span>
