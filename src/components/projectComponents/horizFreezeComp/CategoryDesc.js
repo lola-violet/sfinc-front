a6cf4bd7051457c84446f6e5dfd8134fb63d3bef
Lola Applegate <laurenemilya@gmail.com>
2022-09-22 11:30:22 -0700

    created category description & cutsheet carousel for horizontal freezing

---
+++ src/components/projectComponents/horizFreezeComp/CategoryDesc.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Page Components
+
+
+
+export default function CategoryDescription() {
+    return (
+        <div>
+            <h1>
+                Horizontal Freezing
+            </h1>
+            <p>
+                Ground freezing has been used successfully around the world in the mining industry for over a hundred years.  Many thousands of tons of ore, precious stones and metals would otherwise be inaccessible if not for the application of freeze technology to gain access or to limit groundwater infiltration. SoilFreeze has helped develop site specific freeze systems for gold mines, nickel/lead mines, and others. One of the latest developments is the use of ground freezing technology to assist in the oil shale, oil sands and oil fracking industries. 
+            </p>
+            {/* <h5>Design Considerations</h5> */}
+            <p>
+                ***Image***
+            </p>
+        </div>
+    )
+}
---
+++ src/components/projectComponents/horizFreezeComp/CutsheetCarousel.js
@@ -0,0 +1,93 @@
+import React from 'react';
+
+// Horizontal Freezing
+//// Cutsheets
+import NewarkShaftRepair1201 from '../../../assets/cutsheets/shaCroPass/newark_shaft_repair-1201.png';
+import NewarkTbmReceivingShaft from '../../../assets/cutsheets/shaCroPass/newark_tbm_receiving_shaft.png';
+import PierSLongBeach from '../../../assets/cutsheets/shaCroPass/pier-s_long_beach.png';
+import TurkeyCreekTunnel from '../../../assets/cutsheets/shaCroPass/turkey_creek_tunnel.png';
+
+export default function CutsheetCarousel() {
+    return (
+        <div>
+            <div id='shaCroPassCarousel' className='carousel carousel-dark slide customCarou' data-bs-ride='carousel' data-bs-pause='false'>
+                <div className='carousel-indicators'>
+                    <button
+                        type='button'
+                        data-bs-target='#shaCroPassCarousel'
+                        data-bs-slide-to='0'
+                        className='active'
+                        aria-current='true'
+                        aria-label='slide 1'
+                    ></button>
+                    <button
+                        type='button'
+                        data-bs-target='#shaCroPassCarousel'
+                        data-bs-slide-to='1'
+                        aria-label='slide 2'
+                    ></button>
+                    <button
+                        type='button'
+                        data-bs-target='#shaCroPassCarousel'
+                        data-bs-slide-to='2'
+                        aria-label='slide 3'
+                    ></button>
+                    <button
+                        type='button'
+                        data-bs-target='#shaCroPassCarousel'
+                        data-bs-slide-to='3'
+                        aria-label='slide 4'
+                    ></button>
+                </div>
+                <div className='carousel-inner'>
+                    <div className='carousel-item active'>
+                        <img 
+                            className='d-block w-100 custCarouImg'
+                            src={NewarkShaftRepair1201} 
+                            alt='Cutsheet for Newark Shaft Repair' 
+                        />
+                    </div>
+                    <div className='carousel-item'>
+                        <img
+                            className='d-block w-100 custCarouImg'
+                            src={NewarkTbmReceivingShaft}
+                            alt='Cutsheet for Newark TBM Receiving Shaft' 
+                        />
+                    </div>
+                    <div className='carousel-item'>
+                        <img
+                            className='d-block w-100 custCarouImg'
+                            src={PierSLongBeach}
+                            alt='Cutsheet for Pier-S Long Beach' 
+                        />
+                    </div>
+                    <div className='carousel-item'>
+                        <img
+                            className='d-block w-100 custCarouImg'
+                            src={TurkeyCreekTunnel}
+                            alt='Cutsheet for Turkey Creek Tunnel' 
+                        />
+                    </div>
+                </div>
+                <button
+                    className='carousel-control-prev'
+                    type='button'
+                    data-bs-target='#shaCroPassCarousel'
+                    data-bs-slide='prev'
+                    >
+                    <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Previous</span>
+                </button>
+                <button
+                    className='carousel-control-next'
+                    type='button'
+                    data-bs-target='#shaCroPassCarousel'
+                    data-bs-slide='next'
+                    >
+                    <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Next</span>
+                </button>
+            </div>
+        </div>
+    )
+}
