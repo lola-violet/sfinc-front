e2a56b97e027da65ce3a0f624105bb18fbf74650
Lola Applegate <laurenemilya@gmail.com>
2022-09-22 11:24:03 -0700

    created category description page for utils & pump stations

---
+++ src/components/projectComponents/utilPumpStaComp/CategoryDesc.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Page Components
+
+
+
+export default function CategoryDescription() {
+    return (
+        <div>
+            <h1>
+                Utilities & Pump Stations
+            </h1>
+            <p>
+                We are specialists in providing cost-effective, minimally invasive, impermeable shoring for the installation of lift stations and other underground structures. Freezing is superior to conventional shoring for eliminating lateral groundwater movement into the excavation. Our versatile system can be shaped, as necessary, can incorporate existing utilities so that rerouting is not necessary, and can work around underground obstructions. When dewatering is limited or costly due to quantity to local regulations, soil contaminate treatment, or possible existing structure damage from settlement, the SoilFreeze system is a solution.  
+            </p>
+            <h5>Design Considerations</h5>
+            <p>
+                Existing utilities or known obstructions are addressed in the design phase by locating vertical freeze pipes on either side of the utility or obstruction or by using angled pipes to close the shoring wall below. As in all other applications where a “dry hole” is desired, the design engineer analyzes all strata below the groundwater table to determine the depth required for the unfrozen “plug.” Specialized engineering and construction experiences are critical to provide the most cost-effective, successful solution. Freeze pipes installed too deep will add unnecessary costs to your project, too shallow will cause unnecessary dewatering.
+            </p>
+        </div>
+    )
+}
