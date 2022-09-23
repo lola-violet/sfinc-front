8618b821739965d305dc73fb1217bd78be466073
Lola Applegate <laurenemilya@gmail.com>
2022-09-23 15:08:43 -0700

    updated project category button styles

--- src/components/projectComponents/ProjectIconBtns.js
+++ src/components/projectComponents/ProjectIconBtns.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
+// CSS
+import '../../styles/projects.css';
+
 // Icon Image Imports
 import ShaCrossPassIcon from '../../assets/img/pj_icons_blue/tunneling_cross_passages_icon.png';
 import FoundaExcaIcon from '../../assets/img/pj_icons_blue/foundation_excavations_icon.png';
@@ -9,63 +12,69 @@ import UtilPumpStaIcon from '../../assets/img/pj_icons_blue/utilities_pump_stati
 import EnviroSoilIcon from '../../assets/img/pj_icons_blue/environmental_remediation_icon.png';
 import HorizFreezeIcon from '../../assets/img/pj_icons_blue/horizontal_freezing_icon.png';
 
+// import ShaCrossPassIcon from '../../assets/img/pj_icons_white/tunneling_cross_passages_icon.png';
+// import FoundaExcaIcon from '../../assets/img/pj_icons_white/foundation_excavations_icon.png';
+// import UtilPumpStaIcon from '../../assets/img/pj_icons_white/utilities_pump_stations_icon.png';
+// import EnviroSoilIcon from '../../assets/img/pj_icons_white/environmental_remediation_icon.png';
+// import HorizFreezeIcon from '../../assets/img/pj_icons_white/horizontal_freezing_icon.png';
+
 
 export default function ProjectIconBtns() {
     return (
         <div className='d-flex flex-wrap' >
             <Link 
                 to='/projects/shafts-cross-passages' 
-                className='d-flex flex-fill'
+                className='d-flex flex-fill custPjCat'
                 >
                 <Button
-                    className='flex-fill'
+                    className='flex-fill custPjBtn'
                     >
                     <img src={ShaCrossPassIcon} alt='Shafts and Cross-Passages Icon' />
-                    <p>Shafts & Cross-Passages</p>
+                    <p>Shafts &<br></br>Cross-Passages</p>
                 </Button>
             </Link>
             <Link 
                 to='/projects/foundation-excavations' 
-                className='d-flex flex-fill'
+                className='d-flex flex-fill custPjCat'
                 >
                 <Button
-                    className='flex-fill'
+                    className='flex-fill custPjBtn'
                     >
                     <img src={FoundaExcaIcon} alt='Foundation Excavations Icon' />
-                    <p>Foundation Excavations</p>
+                    <p>Foundation<br></br>Excavations</p>
                 </Button>
             </Link>
             <Link 
                 to='/projects/utilities-pump-stations' 
-                className='d-flex flex-fill'
+                className='d-flex flex-fill custPjCat'
                 >
                 <Button
-                    className='flex-fill'
+                    className='flex-fill custPjBtn'
                     >
                     <img src={UtilPumpStaIcon} alt='Utilities and Pump Stations Icon' />
-                    <p>Utilities & Pump Stations</p>
+                    <p>Utilities &<br></br>Pump Stations</p>
                 </Button>
             </Link>
             <Link 
                 to='/projects/environmental-soil-remediation' 
-                className='d-flex flex-fill'
+                className='d-flex flex-fill custPjCat'
                 >
                 <Button
-                    className='flex-fill'
+                    className='flex-fill custPjBtn'
                     >
                     <img src={EnviroSoilIcon} alt='Environmental and Soil Remediation Icon' />
-                    <p>Environmental & Soil Remediations</p>
+                    <p>Environmental &<br></br>Soil Remediations</p>
                 </Button>
             </Link>
             <Link 
                 to='/projects/horizontal-freezing' 
-                className='d-flex flex-fill'
+                className='d-flex flex-fill custPjCat'
                 >
                 <Button
-                    className='flex-fill'
+                    className='flex-fill custPjBtn'
                     >
                     <img src={HorizFreezeIcon} alt='Horizontal Freezing Icon' />
-                    <p>Horizontal Freezing</p>
+                    <p>Horizontal<br></br>Freezing</p>
                 </Button>
             </Link>
         </div>
