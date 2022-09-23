07e09e147beaccd05abe66248234a6d449beee3b
Lola Applegate <laurenemilya@gmail.com>
2022-09-22 18:14:04 -0700

    updated about page navigation with react-bootstrap buttons

--- src/components/aboutComponents/AboutNavigation.js
+++ src/components/aboutComponents/AboutNavigation.js
@@ -1,38 +1,44 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
+// CSS
+import '../../styles/about.css';
 
 export default function AboutNavigation({ currentPage, handlePageChange }) {
     return (
         <div>
-            <button>
-                <a href="/about"
-                        onClick={() => handlePageChange('Overview')}
-                        className={currentPage === 'About' ? 'nav-link custom-navlink nav-active' : 'nav-link custom-navlink'}
-                    > Overview </a>
-            </button>
-            <button>
-                <a href="#leadership"
-                        onClick={() => handlePageChange('Leadership')}
-                        className={currentPage === 'Leadership' ? 'nav-link custom-navlink nav-active' : 'nav-link custom-navlink'}
-                    > Leadership </a>
-            </button>
-            <button>
-                <a href="#company-history"
-                        onClick={() => handlePageChange('CompanyHistory')}
-                        className={currentPage === 'CompanyHistory' ? 'nav-link custom-navlink nav-active' : 'nav-link custom-navlink'}
-                    > Company History & Patents </a>
-            </button>
-            <button>
-                <a href="#freezing-history"
-                        onClick={() => handlePageChange('FreezingHistory')}
-                        className={currentPage === 'FreezingHistory' ? 'nav-link custom-navlink nav-active' : 'nav-link custom-navlink'}
-                    > Ground Freezing History </a>
-            </button>
-            <button>
-                <a href="#safety"
-                        onClick={() => handlePageChange('Safety')}
-                        className={currentPage === 'Safety' ? 'nav-link custom-navlink nav-active' : 'nav-link custom-navlink'}
-                    > Safety </a>
-            </button>
+            <div className='d-flex flex-wrap abtNav'>
+                <Button 
+                    href='#overview' 
+                    onClick={() => handlePageChange('AboutOverview')}
+                    className='flex-fill abtNavBox'
+                    >Overview
+                </Button>
+                <Button 
+                    href='#leadership' 
+                    onClick={() => handlePageChange('Leadership')}
+                    className='flex-fill abtNavBox'
+                    >Leadership
+                </Button>
+                <Button 
+                    to='#company-history' 
+                    onClick={() => handlePageChange('CompanyHistory')} 
+                    className='flex-fill abtNavBox'
+                    >Company History & Patents
+                </Button>
+                <Button 
+                    to='#freezing-history' 
+                    onClick={() => handlePageChange('FreezingHistory')} 
+                    className='flex-fill abtNavBox'
+                    >Ground Freezing History
+                </Button>
+                <Button 
+                    to='#safety' 
+                    onClick={() => handlePageChange('Safety')} 
+                    className='flex-fill abtNavBox'
+                    >Safety
+                </Button>
+            </div>
         </div>
     )
 }
