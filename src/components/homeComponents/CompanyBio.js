b8ab6e5b84cfbfdfd38e3a0b8df67f836f660567
Lola Applegate <laurenemilya@gmail.com>
2022-09-27 11:15:50 -0700

    updated structure for homepage

--- src/components/homeComponents/CompanyBio.js
+++ src/components/homeComponents/CompanyBio.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 export default function CompanyBio() {
     return (
-        <div>
-            <h1>Applied Innovation. Proven Solutions.</h1>
-            <p>SoilFreeze is the only U.S. construction company specializing exclusively in ground freezing services for the construction and environmental industries.</p>
-            <p>Since 1998, SoilFreeze has helped contractors and engineers successfully complete scores of public and privately owned projects safely, efficiently, and on schedule. We provide deep shaft and utility shoring, soil remediation, and ground stabilization in even the most difficult soil conditions.</p>
+        <div className='container'>
+            <div className='welcomeBlock'>
+                <h1>Welcome to SoilFreeze</h1>
+                <br></br>
+                <h2>Applied Innovation. Proven Solutions.</h2>
+                <p>
+                    SoilFreeze is the only U.S. construction company specializing exclusively in ground freezing services for the construction and environmental industries.
+                </p>
+                <p>
+                    Since 1998, SoilFreeze has helped contractors and engineers successfully complete scores of public and privately owned projects safely, efficiently, and on schedule. We provide deep shaft and utility shoring, soil remediation, and ground stabilization in even the most difficult soil conditions.
+                </p>
+            </div>
         </div>
     )
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,45 +2,18 @@ import React from 'react';
 
 // Page Components
 // import ImageSlide from '../homeComponents/ImageSlide';
-// import CompanyBio from '../homeComponents/CompanyBio';
+import CompanyBio from '../homeComponents/CompanyBio';
 // import ProjectIcons from '../homeComponents/ProjectIcons';
-// import BannerNo1 from '../../assets/img/banners/banner_1.jpg';
 
 import '../../styles/home.css';
 
 
 export default function Home() {
     return (
-        // <div className='container'>
-        //     <h1>I'm the home page</h1>
-        //     <ImageSlide />
-        //     <CompanyBio />
-        //     <ProjectIcons />
-        // </div>
-        // <>
-        // <div>
-        //     <img src={BannerNo1} alt='banner' className='img-fluid homeImg' />
-        //     <div>
-        //         <h1>Welcome to SoilFreeze</h1>
-        //     </div>
-        // </div>
-        // </>
         <div>
             <div className='container-fluid bgImg'>
-                <div className='container'>
-                    <div className='welcomeBlock'>
-                        <h1>Welcome to SoilFreeze</h1>
-                        <h3>Applied Innovation. Proven Solutions.</h3>
-                        <p>
-                            SoilFreeze is the only U.S. construction company specializing exclusively in ground freezing services for the construction and environmental industries.
-                        </p>
-                        <p>
-                            Since 1998, SoilFreeze has helped contractors and engineers successfully complete scores of public and privately owned projects safely, efficiently, and on schedule. We provide deep shaft and utility shoring, soil remediation, and ground stabilization in even the most difficult soil conditions.
-                        </p>
-                    </div>
-                </div>
+                <CompanyBio />
             </div>
         </div>
-        
     )
 }
